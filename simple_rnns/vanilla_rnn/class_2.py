003a7f2f0849bbc558c6a0295b9f2c27d8e27b36
eubinecto <eubinecto>
2021-09-08 10:34:26 +0900

    class_2  완성

--- simple_rnns/vanilla_rnn/class_2.py
+++ simple_rnns/vanilla_rnn/class_2.py
@@ -4,6 +4,8 @@
 
 import torch
 from typing import List, Tuple
+from torch.nn import functional as F
+from torch.utils.data import Dataset, DataLoader
 from keras_preprocessing.text import Tokenizer
 from keras_preprocessing.sequence import pad_sequences
 
@@ -31,8 +33,9 @@ class SimpleRNN(torch.nn.Module):
         # (N, L)
         # -> embeding layer -> (N, E) = x_t
         # x_t * W_xh = (N, E) * (E, H) =  (N, H)
-        self.W_hh = torch.nn.Linear(in_features=hidden_size, out_features=hidden_size)  # (A, B)
-        self.W_xh = torch.nn.Linear(in_features=embed_size, out_features=hidden_size)  # (C, D)
+        self.W_hh = torch.nn.Linear(in_features=hidden_size, out_features=hidden_size)  # (H, H)
+        self.W_xh = torch.nn.Linear(in_features=embed_size, out_features=hidden_size)  # (E, H)
+        self.W_hy = torch.nn.Linear(in_features=hidden_size, out_features=1)  # (H, 1)
 
     def forward(self, X: torch.Tensor) -> torch.Tensor:
         """
@@ -53,7 +56,8 @@ class SimpleRNN(torch.nn.Module):
         # # for loop
         H_t = torch.zeros(size=(X.shape[0], self.hidden_size))
         for t in range(X.shape[1]):
-            H_t = torch.tanh(H_t * self.W_hh + X[:, t] * self.W_xh)
+            Embed_t = self.Embedding(X[:, t])
+            H_t = torch.tanh(self.W_hh(H_t) + self.W_xh(Embed_t))
         # many to one (sentiment analysis)
         return H_t
 
@@ -66,8 +70,16 @@ class SimpleRNN(torch.nn.Module):
         # 배치가 들어왔을 때, 로스를 계산하면 된다.
         H_last = self.forward(X)  # (N, L) -> (N, H)
         # 이것으로부터, 긍정 / 부정일 확률을 구하고 싶다.
-        # (N, H) * ??? -> (N, 1) - 어떻게 할 수 있을까?
-
+        # (N, H) * ???-(H, 1) -> (N, 1) - 어떻게 할 수 있을까?
+        Hy = self.W_hy(H_last)  # (N, H) * (H, 1) -> (N, 1)
+        # 그럼 Hy 의 범위가  [0, 1]?  아니다.
+        # (N, 1)
+        Hy_normalized = torch.sigmoid(Hy)  # [-inf, inf] -> [0, 1]
+        # (N, 1), (N,) -> 하나로 통일하기 위해, y의 맞춘다.
+        Hy_normalized = torch.reshape(Hy_normalized, y.shape)
+        # N개의 로스를 다 더해서, 이 배치의 로스를 구한다.
+        loss = F.binary_cross_entropy(input=Hy_normalized, target=y).sum()
+        return loss
 
 # 어떤 데이터로 학습할까?
 # 10개만 받겠습니다.
@@ -96,6 +108,46 @@ DATA: List[Tuple[str, int]] = [
 ]
 
 
+# builders - 데이터를 텐서로 변환해주는 함수.
+def build_X(sents: List[str], tokenizer: Tokenizer) -> torch.Tensor:
+    tokenizer.fit_on_texts(sents)
+    seqs = tokenizer.texts_to_sequences(sents)
+    # 병국님, 영성님 - 패딩처리가 필요하다. 문장의 길이가 다 다르므로, 규격의 통일이 필요하다.
+    seqs = pad_sequences(sequences=seqs, padding='post', value=0)
+    return torch.LongTensor(seqs)
+
+
+def build_y(labels: List[int]) -> torch.Tensor:
+    return torch.FloatTensor(labels)
+
+
+# 파이토치에서는 데이터셋을 정의해준느 것이 편하다.
+class SimpleDataset(Dataset):
+    def __init__(self, X: torch.Tensor, y: torch.Tensor):
+        self.X = X
+        self.y = y
+
+    # 첫번째 - 데이터 셋의 크기를 알려주는 함수.
+    def __len__(self) -> int:
+        return self.y.shape[0]
+
+    def __getitem__(self, item: int) -> Tuple[torch.Tensor, torch.Tensor]:
+        return self.X[item], self.y[item]
+
+
+class SentimentAnalyzer:
+    def __init__(self, rnn: SimpleRNN, tokenizer: Tokenizer):
+        self.rnn = rnn
+        self.tokenizer = tokenizer
+
+    def __call__(self, text: str) -> float:
+        X = build_X(sents=[text],tokenizer=self.tokenizer)
+        Ht = self.rnn.forward(X)
+        Hy = self.rnn.W_hy(Ht)
+        Hy_normalized = torch.sigmoid(Hy)
+        return Hy_normalized.item()
+
+
 def main():
     # 문장만,
     sents = [
@@ -110,15 +162,43 @@ def main():
     # 정무님: sents: List[str] -> List[int]
     tokenizer = Tokenizer(char_level=True)
     tokenizer.fit_on_texts(sents)
-    seqs = tokenizer.texts_to_sequences(sents)
-    for seq in seqs:
-        print(seq)
 
-    # 병국님, 영성님 - 패딩처리가 필요하다. 문장의 길이가 다 다르므로, 규격의 통일이 필요하다.
-    seqs = pad_sequences(sequences=seqs, padding='post', value=0)
-    print(seqs)
+    # 데이터 구축
+    X = build_X(sents, tokenizer)
+    y = build_y(labels)
+    dataset = SimpleDataset(X, y)  # 파이토치 데이터 셋에 텐서를 저장.
+    dataloader = DataLoader(dataset=dataset, batch_size=5, shuffle=True)
 
+    # 모델 만들기
+    vocab_size = len(tokenizer.word_index) # 정수 1부터 부여.
+    vocab_size += 1  # 왜 하나 더 늘려야할까? - padding token 0.
+    rnn = SimpleRNN(vocab_size=vocab_size, hidden_size=16, embed_size=8)
 
+    # 어떤 최적화 알고리즘을 쓸까?
+    optimizer = torch.optim.Adam(params=rnn.parameters(), lr=0.001)
+
+    # 에폭 루프
+    for e_idx in range(200):
+        # 배치루프
+        losses = list()
+        for b_idx, batch in enumerate(dataloader):
+            X, y = batch
+            # 영성님 - 학습을 해주어야 한다.
+            # 그럼 무엇을 계산해야하는가
+            loss: torch.Tensor = rnn.training_step(X, y)
+            loss.backward()  # 오차역전파 (모든 기울기를 다 계산)
+            # 가중치를 경사도 하강법을 업데이트
+            optimizer.step()
+            # 기울기가 축적이 되는 것을 방지하기 위해 리셋
+            optimizer.zero_grad()
+            losses.append(loss.item())
+        avg_loss = (sum(losses) / len(losses))
+        print("epoch:{}, avg_loss:{}".format(e_idx, avg_loss))
+
+        analyser = SentimentAnalyzer(rnn, tokenizer)
+
+        print("####-------- inferece ---- -#####")
+        print(analyser(text="오늘 날씨가 좋다"))
 
 
 if __name__ == '__main__':
