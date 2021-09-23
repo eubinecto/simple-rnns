b59f6f0f141fa0775fa61452434e035ca8072256
eubinecto <eubinecto>
2021-09-23 18:27:52 +0900

    class_3 - explore_list_star

--- simple_rnns/transformer/examples/explore_list_star.py
+++ simple_rnns/transformer/examples/explore_list_star.py
@@ -4,9 +4,19 @@ def add(a, b):
     return a + b
 
 
+
+def add_too_much(a, b, c, d, e, f, g, h, i, j):
+    return a + b + c + d + e + f + g + h + i + j
+
 def main():
     print(add(1, 2))
+    # print(add([1, 2]))  # 오류
     print(add(*[1, 2]))
+    print(add(**{'a': 1, 'b': 2}))
+    # print(add(a=1, b=/))
+    params = list(range(10))
+    print(add_too_much(*params))
+
 
 if __name__ == '__main__':
     main()
