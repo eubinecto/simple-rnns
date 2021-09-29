e49700b31d3d6ba8caf6088947370c9eaca9d764
eubinecto <eubinecto>
2021-09-29 17:41:28 +0900

    class_5 - multiheadselfattention

--- simple_rnns/transformer/class_5.py
+++ simple_rnns/transformer/class_5.py
@@ -5,16 +5,18 @@ import numpy as np
 import torch
 
 
-class SelfAttentionLayer(torch.nn.Module):
+class MultiHeadSelfAttentionLayer(torch.nn.Module):
     # 적어도 두개의 methods
-    def __init__(self, embed_size: int, hidden_size: int):
+    def __init__(self, embed_size: int, hidden_size: int, heads: int):
         super().__init__()
         # 1 가중치를 정의하는 곳
         # 학습하는 해야하는 가중치???
+        self.heads = heads
         self.hidden_size = hidden_size
         self.W_q = torch.nn.Linear(embed_size, hidden_size)
         self.W_k = torch.nn.Linear(embed_size, hidden_size)
         self.W_v = torch.nn.Linear(embed_size, hidden_size)
+        self.W_o = torch.nn.Linear(hidden_size, hidden_size)
 
     # 2. 신경망의 출력을 정의하 곳
     def forward(self, X_embed: torch.Tensor) -> torch.Tensor:
@@ -26,7 +28,7 @@ class SelfAttentionLayer(torch.nn.Module):
           [[.아.], [.아.], [.0....]]
         ]
         :param X_embed: (N=배치의 크기, L=문장의 길이, E=임베딩 벡터의 크기)
-        :return:
+        :return:  (N, L, H)
         """
         # 계산해야하는 것?
         Q = self.W_q(X_embed)  # (N, L, E) * (?,?) -> (N, L, H)
@@ -36,18 +38,26 @@ class SelfAttentionLayer(torch.nn.Module):
         # (N, L, E) * (N, ?, ?) = (..., L, E) * (..., ?=E, ?=H) -> (..., L, H)
         K = self.W_k(X_embed)  # (N, L, E) * ?=E, ?=H)) -> (N, L, H)
         V = self.W_v(X_embed)  # (N, L, E) * ?=E, ?=H)) -> (N, L, H)
-        Out = self.scaled_dot_product_attention(Q, K, V)
-        return Out
+        # single head - (N, L, H) -> (N, Heads, L, H / Heads)
+        N, L, H = Q.size()
+        # 성분을 재배치, 단 heads차원을 추가.
+        Q = Q.reshape(N, self.heads, L, self.hidden_size // self.heads)
+        K = K.reshape(N, self.heads, L, self.hidden_size // self.heads)
+        V = V.reshape(N, self.heads, L, self.hidden_size // self.heads)
+        Out_ = self.scaled_dot_product_attention(Q, K, V)  # (N, Heads, L, H / heads) -> (N, Heads, L, H / heads)
+        Out_ = Out_.reshape(N, L, H)  # (N, Heads, L, H / heads) -> (N, L, H)
+        Out_ = self.W_o(Out_)  # (N, L, H) * (?=H, ?=H) -> (N, L, H)
+        return Out_
 
     def scaled_dot_product_attention(self,
                                      Q: torch.Tensor,
                                      K: torch.Tensor,
                                      V: torch.Tensor) -> torch.Tensor:
         """
-        :param Q: (N, L, H)
-        :param K: (N, L, H)
-        :param V: (N, L, H)
-        :return:
+        :param Q: (N, Heads, L, H / heads)
+        :param K: (N, Heads, L, H / heads)
+        :param V: (N, Heads, L, H / heads)
+        :return: (N, Heads, L, H / heads)
         """
         # 이제는 뭘해야하나?
         # 1. q-k 유사도 게산을 위해, Q * K^T
@@ -55,15 +65,20 @@ class SelfAttentionLayer(torch.nn.Module):
         Q /= np.sqrt(self.hidden_size)
         K /= np.sqrt(self.hidden_size)
         # Out_ = Q @ K.reshape(K.shape[0], K.shape[2], K.shape[1])
-        Out_ = torch.einsum("nah,nbh->nab", Q, K)  # (N, L, H), (N, L, H) ->  (N, L, H)  * (N, H, L)  -> (N, L, L)
+        # Out_ = torch.einsum("nah,nbh->nab", Q, K)  # (N, L, H), (N, L, H) ->  (N, L, H)  * (N, H, L)  -> (N, L, L)
+        # heads = e
+        # H / heads = x
+        Out_ = torch.einsum("neax,nebx->neab", Q, K)  # (N, Heads, L, H / heads), (N, Heads, L, H / heads) ->  (N, heads, L, L)
         # 다음으로는 뭘해야하나? -> 스케일링
         # Out_ = Out_ / np.sqrt(self.hidden_size)  # 여기에는 논리적인 오류가 있다. 소 잃고 외양간 고치는 격.
         # 다음으로는 softmax로 유사도 행렬을 확룰값으로
-        Out_ = torch.softmax(Out_, dim=1)  # (N, L, L)
+        # Out_ = torch.softmax(Out_, dim=1)  # (N, L, L)
+        Out_ = torch.softmax(Out_, dim=2)  # (N, Heads, L, L)
         # 다음으로는 뭘 계산?
         #  softmax(...) * V
         # 목적? -
-        Out = torch.einsum("nll,nlh->nlh", Out_, V)  # (N, L, L) * (N, L,H) -> (N, L , H)
+        # Out = torch.einsum("nll,nlh->nlh", Out_, V)  # (N, L, L) * (N, L,H) -> (N, L , H)
+        Out = torch.einsum("nell,nelx->nelx", Out_, V)  # (N, Heads, L, L) * (N, Heads, L, H / heads) -> (N, Heads, L, H / heads)
         return Out
 
 
